219ead1f30543dd98b19b93c069a4fab49d0132d
MariaAbuor <maryabuor@gmail.com>
2023-03-15 21:19:28 +0300

    Added some java classes and an interface to read data from MongoDB database

---
+++ src/main/java/com/javaproject1/movies/Review.java
@@ -0,0 +1,17 @@
+package com.javaproject1.movies;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.http.StreamingHttpOutputMessage;
+
+@Document(collection = "reviews")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Review {
+    private ObjectId id;
+    private String body;
+}
